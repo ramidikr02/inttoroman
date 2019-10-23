f6a11f4b1576e92ed1641b2a09595b7b80d5b247
Karnakar <ramidikr02@gmail.com>
2019-10-23 20:11:40 +0530

    adding the logic for generating the number

--- src/main/java/com/test/inttoroman/logic/IntToRoman.java
+++ src/main/java/com/test/inttoroman/logic/IntToRoman.java
@@ -4,6 +4,21 @@ public class IntToRoman {
 
       public static String convert(int number){
 
-        return "";
+        String m[] = {"", "M", "MM", "MMM"};
+        String c[] = {"", "C", "CC", "CCC", "CD", "D",
+            "DC", "DCC", "DCCC", "CM"};
+        String x[] = {"", "X", "XX", "XXX", "XL", "L",
+            "LX", "LXX", "LXXX", "XC"};
+        String i[] = {"", "I", "II", "III", "IV", "V",
+            "VI", "VII", "VIII", "IX"};
+
+        String thousands = m[number/1000];
+        String hundereds = c[(number%1000)/100];
+        String tens = x[(number%100)/10];
+        String ones = i[number%10];
+
+        String ans = thousands + hundereds + tens + ones;
+
+        return ans;
       }
 }
